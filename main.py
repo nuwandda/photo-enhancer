ac89fa3946f9d5450b18d150cd0783c6a3ab832a
Bugrahan Donmez <donmezbugrahan@gmail.com>
2024-03-20 22:58:58 +0300

    Add command line script

---
+++ main.py
@@ -0,0 +1,34 @@
+from PIL import Image
+import numpy as np
+from enhancer import Enhancer
+import argparse
+
+
+def main(method, image_path, output_path, background_enhancement, upscale):
+    # Create enhancer
+    enhancer = Enhancer(method=method, background_enhancement=background_enhancement, upscale=upscale)
+
+    image = np.array(Image.open(image_path))
+    restored_image = enhancer.enhance(image)
+
+    final_image = Image.fromarray(restored_image)
+    final_image.save(output_path)
+
+
+if __name__ == "__main__":
+    # Create argument parser
+    parser = argparse.ArgumentParser(description="Process method and image.")
+    # Add method argument
+    parser.add_argument("--method", type=str, required=True, help="Specify the enhance method. (gfpgan, RestoreFormer, codeformer)")
+    # Add image path argument
+    parser.add_argument("--image_path", type=str, required=True, help="Specify the image path.")
+    # Add output image argument
+    parser.add_argument("--output_path", type=str, required=True, help="Specify the output path.")
+    # Add background enhancement argument
+    parser.add_argument("--background_enhancement", action="store_true", help="Specify the background enhancement option.", default=True)
+    # Add enhancement upscale argument
+    parser.add_argument("--upscale", type=int, help="Specify the enhancement scale (2, 4).")
+    # Parse the arguments
+    args = parser.parse_args()
+    # Call the main function with parsed arguments
+    main(args.method, args.image_path, args.output_path, args.background_enhancement, args.upscale)
